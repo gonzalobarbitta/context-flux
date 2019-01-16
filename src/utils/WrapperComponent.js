fcbf9ce0adffbc65d4fd4db8f554e2464bb23f97
Gonzalo Barbitta <gonzalo.barbitta@gmail.com>
2019-01-16 15:47:47 -0300

    Added propTypes to WrapperComponent

--- src/utils/WrapperComponent.js
+++ src/utils/WrapperComponent.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 import shallowCompare from './shallowCompare';
 
 class WrapperComponent extends React.Component {
-    shouldComponentUpdate(nextProps) {
-      const { context, mapStateToProps } = this.props;
-      return shallowCompare(mapStateToProps(context), mapStateToProps(nextProps.context));
-    }
-    render() {
-      return (
-        <React.Fragment>
-          { this.props.children }
-        </React.Fragment>
-      );
-    }
+  shouldComponentUpdate(nextProps) {
+    const { context, mapStateToProps } = this.props;
+    return shallowCompare(mapStateToProps(context), mapStateToProps(nextProps.context));
+  }
+  render() {
+    return (
+      <React.Fragment>
+        { this.props.children }
+      </React.Fragment>
+    );
+  }
 }
 
-export default WrapperComponent;
+WrapperComponent.propTypes = {
+  context: PropTypes.object.isRequired,
+  mapStateToProps: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
+export default WrapperComponent;
